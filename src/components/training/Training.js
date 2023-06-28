b5ae14eabadc664d55e97cce627cc7237300e59b
EddieBaum <115045326+EddieBaum@users.noreply.github.com>
2023-06-27 20:20:52 -0500

    play around with training page, no major changes

--- src/components/training/Training.js
+++ src/components/training/Training.js
@@ -1,30 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./training.css";
-import TrainingComp from './TrainingComp'; 
+import TrainingComp from "./TrainingComp";
+import anime from "animejs";
+
+
 
 function Training() {
-  const principles = [
-    "Movement",
-    "Sun",
-    "Food",
-    "Sleep",
-    "Relationships",
-  ];
+  const principles = ["Movement", "Sun", "Food", "Sleep", "Relationships"];
+
+  useEffect(() => {
+    var textWrappers = document.querySelectorAll(".ml3 ");
+
+    textWrappers.forEach((textWrapper) => {
+      textWrapper.innerHTML = textWrapper.textContent.replace(
+        /\S/g,
+        "<span class='letter'>$&</span>"
+      );
+    });
+
+    anime
+      .timeline({ loop: true })
+      .add({
+        targets: ".ml3 .letter",
+        opacity: [0, 1],
+        easing: "easeInOutQuad",
+        duration: 2250,
+        delay: (el, i) => 150 * (i + 1),
+      })
+      .add({
+        targets: ".ml3",
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000,
+      });
+  }, []); // closing brace was here
 
   return (
-      <div className="training-parent-div">
-          <p className='training-p'>Life's quality and health are upheld by five essential pillars: Movement, Sunlight, Sleep, Food Quality, and Relationships. Movement is our vitality, pushing us towards growth. Sunlight connects us to nature, vital for our wellbeing. Sleep, a restorative power, is crucial for rejuvenation. Food Quality impacts our health, shaping our body and mind. Relationships, fundamental to our emotional state, remind us of the value of connection and love. Nurturing these pillars fosters conditions for a thriving life.</p>
-      <div className="five-columns-container">
+    <div className="training-parent-div">
+        <h1 className="ml3">Movement.</h1>
+      <h1 className="ml3">Sleep.</h1>
+      <h1 className="ml3">Nutrition.</h1>
+      <h1 className="ml3">Sunlight.</h1>
+      <h1 className="ml3">Relationships.</h1>
+    </div>
+  );
+}
+
+export default Training;
+
+/* 
         {principles.map((principle, index) => (
           <div key={index} className="column">
             <h2>{principle}</h2>
             <p>Description of {principle}</p>
           </div>
         ))}
-          </div>
-          <TrainingComp />
-    </div>
-  );
-}
-
-export default Training;
+        */
